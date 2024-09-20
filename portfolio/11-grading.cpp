4c75698cf8f8ffeb1c4088a4b5aff47da4f12cd2
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2024-09-20 10:55:48 -0600

    Create 11-grading.cpp

---
+++ portfolio/11-grading.cpp
@@ -0,0 +1,114 @@
+/**
+ * ==============================================================================
+ * Author: Michael Gene Brockus (Dreamer)
+ * Email: michaelbrockus@gmail.com
+ * Organization: Fossil Logic
+ * Description: 
+ *     This file is part of the Fossil Logic project, where innovation meets
+ *     excellence in software development. Michael Gene Brockus, also known as
+ *     "Dreamer," is a dedicated contributor to this project. For any inquiries,
+ *     feel free to contact Michael at michaelbrockus@gmail.com.
+ * ==============================================================================
+ */
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include <stdexcept>
+
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+vector<int32_t> gradingStudents(vector<int32_t> grades) {
+    try {
+        if (grades.empty()){
+            throw std::invalid_argument("Input array must not be empty.");
+        }
+
+        for (auto iter = 0U; iter < grades.size(); iter++) {
+            if (grades[iter] >= 38) {
+                switch (grades[iter] % 5) {
+                    case 4:
+                        grades[iter] += 1;
+                        break;
+                    case 3:
+                        grades[iter] += 2;
+                        break;
+                }
+            }
+        }
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Invalid argument error: " << e.what() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+    }
+    return grades;
+}
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string grades_count_temp;
+    getline(cin, grades_count_temp);
+
+    int grades_count = stoi(ltrim(rtrim(grades_count_temp)));
+
+    vector<int> grades(grades_count);
+
+    for (int i = 0; i < grades_count; i++) {
+        string grades_item_temp;
+        getline(cin, grades_item_temp);
+
+        int grades_item = stoi(ltrim(rtrim(grades_item_temp)));
+
+        grades[i] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
